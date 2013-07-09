eeca5c3a21d913c61acffcee8a3fbfced9d1eed8
aywu <aywu477@gmail.com>
2013-07-09 10:55:43 -0700

    Initialized app with pom.xml and new cddApp package.
    Signed-off-by: aywu <aywu477@gmail.com>

---
+++ sr/main/java/edu/ucsf/rbvi/cddApp/internal/CyActivator.java
@@ -0,0 +1,48 @@
+package edu.ucsf.rbvi.cddApp.internal;
+
+import static org.cytoscape.work.ServiceProperties.COMMAND;
+import static org.cytoscape.work.ServiceProperties.COMMAND_NAMESPACE;
+import static org.cytoscape.work.ServiceProperties.INSERT_SEPARATOR_BEFORE;
+import static org.cytoscape.work.ServiceProperties.IN_MENU_BAR;
+import static org.cytoscape.work.ServiceProperties.MENU_GRAVITY;
+import static org.cytoscape.work.ServiceProperties.PREFERRED_MENU;
+import static org.cytoscape.work.ServiceProperties.TITLE;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Set;
+
+import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.task.NetworkViewTaskFactory;
+import org.cytoscape.task.read.LoadVizmapFileTaskFactory;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+import org.cytoscape.view.vizmap.VisualStyle;
+import org.cytoscape.work.TaskFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CyActivator extends AbstractCyActivator {
+	private static Logger logger = LoggerFactory
+			.getLogger(edu.ucsf.rbvi.cddApp.internal.CyActivator.class);
+
+	public CyActivator() {
+		super();
+	}
+
+	public void start(BundleContext bc) {
+
+		// See if we have a graphics console or not
+		boolean haveGUI = true;
+		ServiceReference ref = bc.getServiceReference(CySwingApplication.class.getName());
+
+		if (ref == null) {
+			haveGUI = false;
+			// Issue error and return
+		}
+	}
+}
